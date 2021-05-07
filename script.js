1cb68028e5c8a66df3143d7b28696e216755138c
David Keller <kellerdb@gmail.com>
2021-05-07 13:13:48 -0700

    formatting

--- script.js
+++ script.js
@@ -6,31 +6,40 @@ let coins = [1, 5, 10, 25]
 
 coinsButton.addEventListener(`click`, showCoins)
 
+cents.addEventListener(`keydown`, keyPressed)
+cents.focus()
+
 function showCoins() {
   let centsValue = cents.value.trim()
 
-  if (centsValue != `` && !isNaN(centsValue)) {
-    if (centsValue > 99) {
-      box.innerHTML = `Number of cents cannot be more than 99.`
-    }
-    else {
-      box.innerHTML = ``
+  if (centsValue > 99) {
+    box.innerHTML = `Number of cents cannot be more than 99.`
+  } else {
+    box.innerHTML = ``
 
-      while (centsValue >= 1) {
-        let selectedCoin
-        
-        for (let coin of coins) {
-          if (centsValue >= coin) {
-            selectedCoin = coin
-          }
+    while (centsValue >= 1) {
+      let selectedCoin
+
+      for (let coin of coins) {
+        if (centsValue >= coin) {
+          selectedCoin = coin
         }
-
-        let image = document.createElement(`img`)
-        image.src = `${selectedCoin}.png`
-        box.appendChild(image)
-
-        centsValue = centsValue - selectedCoin
       }
+
+      let image = document.createElement(`img`)
+      image.src = `${selectedCoin}.png`
+      box.appendChild(image)
+
+      centsValue = centsValue - selectedCoin
     }
+  }
+
+  cents.value = ``
+  cents.focus()
+}
+
+function keyPressed(event) {
+  if (event.keyCode == 13) {
+    showCoins()
   }
 }
